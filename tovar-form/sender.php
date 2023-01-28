d077104a845330c392771e26c494ae318311f120
Romka21312 <106865957+Romka21312@users.noreply.github.com>
2023-01-28 11:58:43 +0700

    Add files via upload

---
+++ tovar-form/sender.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+	$phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+	// $to = "Roman10.shulpov@gmail.com"; 
+	// $date = date ("d.m.Y"); 
+	// $time = date ("h:i");
+	// $from = $email;
+	// $subject = "Оформление заказа";
+
+	
+	$msg="
+    Имя: $name /n
+    Фамилия: $surname /n
+    Телефон: $phone /n
+    Почта: $email /n
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
+
+?>
+
+<p>Благодарим за заказ, с вами свяжется специалист</p>
